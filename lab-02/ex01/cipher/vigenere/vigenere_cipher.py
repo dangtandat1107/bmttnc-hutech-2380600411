8b1a0b8b8be436a2129343fb1c66ad7468a07cbc
dangtandat1107 <dangtandat1107@gmail.com>
2026-05-25 10:51:15 +0700

    [add] lab-02 ex01 caesar

--- lab-02/ex01/cipher/vigenere/vigenere_cipher.py
+++ lab-02/ex01/cipher/vigenere/vigenere_cipher.py
@@ -1,33 +1,46 @@
 class VigenereCipher:
     def __init__(self):
-        pass
+        # Tự định nghĩa trực tiếp bảng chữ cái tiếng Anh viết hoa chuẩn
+        self.alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
 
-    def encrypt_text(self, plain_text, key):
-        encrypted_text = ""
+    def encrypt_text(self, plain_text: str, key: str) -> str:
+        alphabet_len = len(self.alphabet)
+        plain_text = plain_text.upper()
+        key = key.upper()
+        encrypted_text = []
         key_index = 0
-        for char in plain_text:
-            if char.isalpha():
-                key_shift = ord(key[key_index % len(key)].upper()) - ord('A')
-                if char.isupper():
-                    encrypted_text += chr((ord(char) - ord('A') + key_shift) % 26 + ord('A'))
-                else:
-                    encrypted_text += chr((ord(char) - ord('a') + key_shift) % 26 + ord('a'))
+        
+        for letter in plain_text:
+            if letter in self.alphabet:
+                letter_index = self.alphabet.index(letter)
+                key_letter = key[key_index % len(key)]
+                key_letter_index = self.alphabet.index(key_letter)
+                
+                output_index = (letter_index + key_letter_index) % alphabet_len
+                encrypted_text.append(self.alphabet[output_index])
                 key_index += 1
             else:
-                encrypted_text += char
-        return encrypted_text
+                encrypted_text.append(letter)
+                
+        return "".join(encrypted_text)
 
-    def decrypt_text(self, encrypted_text, key):
-        decrypted_text = ""
+    def decrypt_text(self, encrypted_text: str, key: str) -> str:
+        alphabet_len = len(self.alphabet)
+        encrypted_text = encrypted_text.upper()
+        key = key.upper()
+        decrypted_text = []
         key_index = 0
-        for char in encrypted_text:
-            if char.isalpha():
-                key_shift = ord(key[key_index % len(key)].upper()) - ord('A')
-                if char.isupper():
-                    decrypted_text += chr((ord(char) - ord('A') - key_shift) % 26 + ord('A'))
-                else:
-                    decrypted_text += chr((ord(char) - ord('a') - key_shift) % 26 + ord('a'))
+        
+        for letter in encrypted_text:
+            if letter in self.alphabet:
+                letter_index = self.alphabet.index(letter)
+                key_letter = key[key_index % len(key)]
+                key_letter_index = self.alphabet.index(key_letter)
+                
+                output_index = (letter_index - key_letter_index) % alphabet_len
+                decrypted_text.append(self.alphabet[output_index])
                 key_index += 1
             else:
-                decrypted_text += char
-        return decrypted_text
+                decrypted_text.append(letter)
+                
+        return "".join(decrypted_text)
